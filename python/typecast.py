dabd8d2d5d5de01e67f45e776b80804455159c0e
Ralein Nova <raleinnova@gmail.com>
2026-03-27 14:01:27 +0530

    typecast

---
+++ python/typecast.py
@@ -0,0 +1,13 @@
+print(str(123))  # Output: '123'
+print(int("456"))  # Output: 456
+print(float("3.14"))  # Output: 3.14
+print(bool(0))  # Output: False         
+print(list("hello"))  # Output: ['h', 'e', 'l', 'l', 'o']
+print(tuple("world"))  # Output: ('w', 'o', 'r', 'l', 'd')
+print(dict([('a', 1), ('b', 2), ('c', 3)]))  # Output: {'a': 1, 'b': 2, 'c': 3}
+print(set([1, 2, 3, 2, 1]))  # Output: {1, 2, 3}
+print(ord('A'))  # Output: 65
+print(chr(65))  # Output: 'A'
+print(hex(255))  # Output: '0xff'
+print(bin(255))  # Output: '0b11111111'
+print(oct(255))  # Output: '0o377'  
